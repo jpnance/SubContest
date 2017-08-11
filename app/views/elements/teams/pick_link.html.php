74f1e2644df889812f4271314fd2b751dcbed658
Patrick Nance <jpnance@gmail.com>
2017-08-11 15:06:13 -0700

    I guess we need to use absolute links now. Not really sure why, to tell you the truth.

--- app/views/elements/teams/pick_link.html.php
+++ app/views/elements/teams/pick_link.html.php
@@ -1,2 +1,2 @@
-<?= $this->html->link($this->team->name($team), 'games/unpick/' . $game->_id, array('id' => $team->abbreviation, 'class' => 'picked')); ?>
-<?= $this->html->link($this->team->name($team), 'games/pick/' . $game->_id . '/' . $team->abbreviation, array('id' => $team->abbreviation, 'class' => 'not-picked')); ?>
+<?= $this->html->link($this->team->name($team), '/games/unpick/' . $game->_id, array('id' => $team->abbreviation, 'class' => 'picked')); ?>
+<?= $this->html->link($this->team->name($team), '/games/pick/' . $game->_id . '/' . $team->abbreviation, array('id' => $team->abbreviation, 'class' => 'not-picked')); ?>
