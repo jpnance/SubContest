c16ca90e6b5fb67d3e31b10aee5227894fde0c03
Patrick Nance <jpnance@gmail.com>
2022-08-30 10:46:24 -0700

    Removing unnecessary route.

--- routes.js
+++ routes.js
@@ -27,7 +27,6 @@ module.exports = function(app) {
 	app.get('/schedule/?', schedule.showAllForDate);
 	app.get('/schedule.json', schedule.allForDate);
 	app.get('/schedule/:week(\\d\\d?)', schedule.showAllForDate);
-	app.get('/schedule/:teamAbbreviation(\\w+)', schedule.showAllForTeam);
 
 	app.get('/pick/:teamId/:gameId', classics.pick);
 	app.get('/unpick/:teamId/:gameId', classics.unpick);
--- services/schedule.js
+++ services/schedule.js
@@ -2,7 +2,6 @@ var User = require('../models/User');
 var Session = require('../models/Session');
 var Game = require('../models/Game');
 var Team = require('../models/Team');
-var Classic = require('../models/Classic');
 
 var dateFormat = require('dateformat');
 
@@ -15,83 +14,6 @@ module.exports.showAll = function(request, response) {
 		var games = values[0];
 
 		response.render('index', { games: games });
-	});
-};
-
-module.exports.showAllForTeam = function(request, response) {
-	Session.withActiveSession(request, function(error, session) {
-		Team.findOne({ abbreviation: request.params.teamAbbreviation }, function(error, team) {
-			if (error) {
-				response.sendStatus(500);
-				return;
-			}
-
-			if (!team) {
-				response.sendStatus(404);
-				return;
-			}
-
-			var dataPromises = [
-				Game.find({ season: process.env.SEASON, '$or': [ { 'home.team': team._id }, { 'away.team': team._id } ]}).sort('startTime').populate('away.team away.probablePitcher home.team home.probablePitcher'),
-				Classic.find({ season: process.env.SEASON }).populate('user team')
-			];
-
-			Promise.all(dataPromises).then(function(values) {
-				var games = values[0];
-				var classics = values[1];
-
-				games.forEach(function(game) {
-					game.away.picks = [];
-					game.home.picks = [];
-
-					classics.forEach(function(classic) {
-						if (session && session.user.username == classic.user.username) {
-							if (classic.team._id == game.away.team._id) {
-								game.away.team.classic = classic;
-							}
-
-							if (classic.team._id == game.home.team._id) {
-								game.home.team.classic = classic;
-							}
-						}
-
-						if (classic.picks.indexOf(game._id) > -1) {
-							if (session && session.user.username == classic.user.username) {
-								game.classic = classic;
-
-								if (classic.team._id == game.away.team._id) {
-									game.away.picked = true;
-								}
-								else if (classic.team._id == game.home.team._id) {
-									game.home.picked = true;
-								}
-							}
-
-							if (game.hasDefinitelyStarted()) {
-								if (classic.team._id == game.away.team._id) {
-									game.away.picks.push(classic);
-								}
-
-								if (classic.team._id == game.home.team._id) {
-									game.home.picks.push(classic);
-								}
-							}
-						}
-					});
-
-					game.away.picks.sort(Classic.populatedUserDisplayNameSort);
-					game.home.picks.sort(Classic.populatedUserDisplayNameSort);
-				});
-
-				var responseData = {
-					session: session,
-					games: games,
-					team: team
-				};
-
-				response.render('schedule/team', responseData);
-			});
-		});
 	});
 };
 
