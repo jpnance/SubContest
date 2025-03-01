00140f3f06bb363be659bba80d1acc225268a721
Patrick Nance <jpnance@gmail.com>
2025-02-28 21:28:15 -0800

    This seems like it's good.

---
+++ backfill.js
@@ -0,0 +1,37 @@
+const dotenv = require('dotenv').config({ path: '/app/.env' });
+
+const mongoose = require('mongoose');
+const mongoUri = process.env.MONGODB_URI || null;
+
+mongoose.connect(mongoUri);
+
+const User = require('./models/User');
+const Game = require('./models/Game');
+
+User.find({}).then(handleUsers).then(console.log).then(disconnect);
+
+function handleUsers(users) {
+	return Promise.all(users.map(convertUsername));
+}
+
+function disconnect() {
+	mongoose.disconnect();
+	process.exit();
+}
+
+function convertUsername(user) {
+	const { firstName, lastName, username } = user;
+	const newUsername = [firstName, lastName].join('-').toLowerCase().replace(/[']/g, '');
+
+	user.username = newUsername;
+
+	const promises = [];
+
+	promises.push(user.save());
+
+	if (username != newUsername) {
+		promises.push(Game.updateMany({ [`picks.${username}`]: { '$exists': true } }, { '$rename': { [`picks.${username}`]: `picks.${newUsername}` } }));
+	}
+
+	return Promise.all(promises);
+}
